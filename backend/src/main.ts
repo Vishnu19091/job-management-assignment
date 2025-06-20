42aa93d753e10b6d83eeee376e7764a2e69ee103
Vishnu19091 <vishnusah368@gmail.com>
2025-06-20 06:59:27 +0530

    Update on 2025-06-20 06:59:27

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { METHODS } from 'http';
+import { Request, Response } from 'express';
 
-const PORT=3001
+let cachedApp: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().setTitle('Job_Management_Admin')
-    .setDescription('The Job Management Admin Interface APi').build();
-  
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Job_Management_Admin')
+    .setDescription('The Job Management Admin Interface API')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   // Enable CORS
   app.enableCors({
-    origin: '*', //set to all
-    methods: 'GET,POST,PUT,DELETE', //allowed methods
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
-    allowedHeaders:'Content-Type, Authorization',
-  })
-  await app.listen(PORT);
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.init(); // Important: Don't use app.listen
+  return app.getHttpAdapter().getInstance(); // return Express instance
 }
-bootstrap();
+
+// 👇 Correct export handler for Vercel
+export default async function handler(req: Request, res: Response) {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+  }
+  return cachedApp(req, res);
+}
