3b637127c2445f389275ba7101ec93e574adccf0
Vishnu19091 <vishnusah368@gmail.com>
2025-06-20 07:28:03 +0530

    Update on 2025-06-20 07:28:03

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-const PORT=3001
+const server = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().setTitle('Job_Management_Admin')
-    .setDescription('The Job Management Admin Interface APi').build();
-  
+async function createNestServer(expressInstance: express.Express) {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Management')
+    .setDescription('Job Admin API')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/api', app, document);
 
-  // Enable CORS
   app.enableCors({
-    origin: '*', //set to all
-    methods: 'GET,POST,PUT,DELETE', //allowed methods
-    credentials: true,
-    allowedHeaders:'Content-Type, Authorization',
-  })
-  await app.listen(PORT);
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.init();
 }
-bootstrap();
+
+createNestServer(server);
+
+export default server;
